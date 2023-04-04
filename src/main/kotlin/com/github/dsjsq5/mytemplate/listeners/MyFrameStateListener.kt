4e96964297818b74f8182ee86590b791a61b6f70
GitHub Action <action@github.com>
2023-04-04 05:12:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/dsjsq5/mytemplate/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/dsjsq5/mytemplate/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.dsjsq5.mytemplate.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
